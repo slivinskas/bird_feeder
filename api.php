706e5286deaa772e448bdf2906fc62db31333194
Slivinskas <slivinskasvytenis@gmail.com>
2017-03-11 13:34:37 +0200

    init

--- api.php
+++ api.php
@@ -5,3 +5,21 @@
  * Date: 2017-03-11
  * Time: 07:56
  */
+
+$config =  include_once ("config.php");
+include_once ("libary/DB.c  lass.php");
+
+$db = new DB($config->host,$config->user,$config->pass,$config->db);
+
+// create a new cURL resource
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "http://www.example.com/");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// grab URL and pass it to the browser
+curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
