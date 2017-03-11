ca3cc7fe85cc7ddffbc42d80b9d8253122bd1dc3
Slivinskas <slivinskasvytenis@gmail.com>
2017-03-12 00:12:04 +0200

    Veikia kameros sukiojimas

--- js/index.js
+++ js/index.js
@@ -9,9 +9,15 @@
 
 })();
 
-function sukames() {
+function sukamesIkaire() {
     $.ajax({
         url: "http://192.168.43.165/decoder_control.cgi?command=4", jsonp: "callback", dataType: "jsonp"
+    });
+}
+
+function sukamesIdesine() {
+    $.ajax({
+        url: "http://192.168.43.165/decoder_control.cgi?command=6", jsonp: "callback", dataType: "jsonp"
     });
 }
 
@@ -19,4 +25,15 @@ function stop() {
     $.ajax({
         url: "http://192.168.43.165/decoder_control.cgi?command=29", jsonp: "callback", dataType: "jsonp"
     });
+}
+
+function ratu() {
+    "use strict";
+    sukamesIkaire();
+    setTimeout(function () {
+        sukamesIdesine();
+        setTimeout(function () {
+            stop();
+        },5000);
+    }, 5000);
 }
